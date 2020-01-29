f4ad1f5226b13add0646b07cfbd9aab9806d64f8
Anis-Barghouthi <anis.barghouthi@outlook.com>
2020-01-29 20:01:08 +0100

    pushing

--- app/src/main/java/tn/esprit/sleam41/MainActivity.java
+++ app/src/main/java/tn/esprit/sleam41/MainActivity.java
@@ -1,42 +1,28 @@
 package tn.esprit.sleam41;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button button_count, button_toast;
-    private TextView show_count;
-    private int nombre=0;
+    private TextView mShow_count;
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        button_count = findViewById(R.id.button_count);
-        button_toast = findViewById(R.id.button_toast);
-        show_count = findViewById(R.id.show_count);
+    public void showToast(View view) {
+        Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT).show();
+    }
 
-        button_count.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nombre++;
-                show_count.setText(String.valueOf(nombre));
-            }
-        });
-
-        button_toast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_SHORT);
-            }
-        });
+    public void countUp(View view) {
 
     }
 }
